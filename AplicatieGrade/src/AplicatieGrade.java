b0b06bf98e753357afd5910ea667a7573d3868db
aroman07 <87022612+aroman07@users.noreply.github.com>
2021-10-26 17:27:16 +0300

    Add files via upload

---
+++ AplicatieGrade/src/AplicatieGrade.java
@@ -0,0 +1,8 @@
+import CalculGrade.*;
+
+public class AplicatieGrade {
+    public static void main(String[] args) {
+        System.out.println(Grade.convertCtoF(100));
+        System.out.println(Grade.convertFtoC(100));
+    }
+}
---
+++ AplicatieGrade/src/CalculGrade/Grade.java
@@ -0,0 +1,11 @@
+package CalculGrade;
+
+public class Grade {
+    public static double convertCtoF(double gradeC){
+        return 0.0;
+    }
+
+    public static double convertFtoC(double gradeF){
+        return 1.0;
+    }
+}
