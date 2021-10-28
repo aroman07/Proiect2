8ed449d3729fc03af00e5e79db03a2b3b83d431b
aromanrsc <93315952+aromanrsc@users.noreply.github.com>
2021-10-28 11:47:00 +0300

    Add files via upload

--- AplicatieGrade/src/AplicatieGrade.java
+++ AplicatieGrade/src/AplicatieGrade.java
@@ -4,5 +4,7 @@ public class AplicatieGrade {
     public static void main(String[] args) {
         System.out.println(Grade.convertCtoF(100));
         System.out.println(Grade.convertFtoC(100));
+        
+        //test
     }
 }
